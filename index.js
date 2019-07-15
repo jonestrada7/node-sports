5203b3f6794551012bea2ec3c45ed46828b04550
jonestrada7 <jonathan.estrada747@gmail.com>
2019-07-14 19:20:38 -0700

    created basic website framework and server functionality

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+// READING FILES
+
+const tempMain = fs.readFileSync(`${__dirname}/templates/template-main.html`, 'utf-8');
+
 // CREATING SERVER
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
     
     // Main package
     if (pathname === '/' || pathname === '/main') {
-        res.end('This is the MAIN');
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        
+        const output = tempMain;
+        res.end(output);
+    
+    // Page not found
     } else {
         res.writeHead(404, { 'Content-type': 'text/html' });
         res.end('<h1>Page not found!</h1>');
